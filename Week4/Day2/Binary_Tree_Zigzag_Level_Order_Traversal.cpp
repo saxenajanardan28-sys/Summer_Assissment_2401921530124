a8e00764e6d2c3090a9bb862ea6b4574ef8d46af
saxenajanardan28-sys <saxenajanardan28@gmail.com>
2026-06-28 23:02:01 +0530

    Create Binary_Tree_Zigzag_Level_Order_Traversal.cpp

---
+++ Week4/Day2/Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+
+        vector<vector<int>> ans;
+
+        if(root == NULL) {
+            return ans;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool leftToRight = true;
+
+        while(!q.empty()) {
+
+            int size = q.size();
+
+            vector<int> level(size);
+
+            for(int i = 0; i < size; i++) {
+
+                TreeNode* frontNode = q.front();
+                q.pop();
+
+                int index;
+
+                if(leftToRight) {
+                    index = i;
+                }
+                else {
+                    index = size - i - 1;
+                }
+
+                level[index] = frontNode->val;
+
+                if(frontNode->left != NULL) {
+                    q.push(frontNode->left);
+                }
+
+                if(frontNode->right != NULL) {
+                    q.push(frontNode->right);
+                }
+            }
+
+            leftToRight = !leftToRight;
+
+            ans.push_back(level);
+        }
+
+        return ans;
+    }
+};
